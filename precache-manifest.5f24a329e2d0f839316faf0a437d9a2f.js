6d6213ed36d29926cd22b3c36e00d2683beacbe1
Jacqueline Silva <jacque.sx@hotmail.com>
2022-08-09 23:53:54 -0300

    Updates

---
+++ precache-manifest.5f24a329e2d0f839316faf0a437d9a2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38904d8abdada7888345da89163eb616",
+    "url": "/trybewallet/index.html"
+  },
+  {
+    "revision": "e9565a3bc9a130e0ff21",
+    "url": "/trybewallet/static/css/main.f096b898.chunk.css"
+  },
+  {
+    "revision": "345ef4e6288795298dd5",
+    "url": "/trybewallet/static/js/2.f15663ec.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trybewallet/static/js/2.f15663ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9565a3bc9a130e0ff21",
+    "url": "/trybewallet/static/js/main.ad9f425f.chunk.js"
+  },
+  {
+    "revision": "5dc72ebdcdec490a7442",
+    "url": "/trybewallet/static/js/runtime-main.ec83dc77.js"
+  }
+]);
